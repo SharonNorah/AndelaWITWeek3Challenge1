f842170dc2f7b1f9da6c1e2f2d137e44aaf59eee
akolsharonnorah@gmail.com <akolsharonnorah@gmail.com>
2019-09-05 22:45:53 +0300

    created new  branch

---
+++ arraysort.js
@@ -0,0 +1,34 @@
+/*sorting
+function that takes the array and returns
+ an object with property names evens, odds, and chars.*/
+function arraySort(anArray) {
+    var even = [];
+    var odd = [];
+    var char = [];
+    if (!Array.isArray(anArray)) {
+        return 'invalid input';
+    }
+    for (i = 0; i < anArray.length; i++) {
+        if (Number.isInteger(anArray[i])) {
+            if (anArray[i] % 2 === 0) {
+                even.push(anArray[i])
+            }
+            if (anArray[i] % 2 === 1) {
+                odd.push(anArray[i])
+            }
+        }
+        if (!Number.isInteger(anArray[i])) {
+
+            if (anArray[i].length > 1) {
+                return 'invalid input';
+            }
+            char.push(anArray[i])
+        }
+    }
+    return { evens: even.sort(), odds: odd.sort(), chars: char.sort() };
+};
+module.exports = arraySort; {
+    console.log(arraySort([2, 0, 6, 5, 1, 7, 'z', 'a']));
+    console.log(arraySort([2, 0, 6, 5, 1, 'sharon']));
+    console.log(arraySort('sharon'));
+}
---
+++ sortedarr.js
@@ -0,0 +1,14 @@
+//function to return sorted array
+function func() {
+
+    //Original string 
+    var arr = [2, 5, 8, 1, 4, 6]
+    var str = ['s', 'h', 'a', 'r', 'n']
+
+    //Sorting the array 
+    console.log(arr.sort());
+    console.log(str.sort());
+
+    //console.log(arr);
+}
+func(); 
